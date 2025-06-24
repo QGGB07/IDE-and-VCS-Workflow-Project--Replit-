a0913262728be46b5a68bf182bd3f71af6577e1a
QGGB07 <qgb23300@email.vccs.edu>
2025-06-24 01:27:53 +0000

    Added Program-2

---
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string name, adress, phoneNumber;
+  name = "Queenie Grethell Balingit";
+  adress = "128 Ponsonby Drive";
+  phoneNumber = "757-768-3124";
+
+  cout << "Name: " << name << endl;
+  cout << "Adress: " << adress << endl;
+  cout << "Phone Number: " << phoneNumber << endl; 
+  return 0;
+}
